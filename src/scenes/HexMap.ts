0e7694a34bd447cfd9b48b366bf2f3c323047c0c
James Pettit <james@academia.edu>
2020-04-27 18:04:03 +0800

    Fine, I'll draw the hexagons myself.

--- src/scenes/HexMap.ts
+++ src/scenes/HexMap.ts
@@ -4,8 +4,8 @@ import GameState from '../GameState';
 export default class HexMap extends Phaser.Scene {
   state?: GameState
   controls?: Phaser.Cameras.Controls.SmoothedKeyControl
-  mapWidth = 60
-  mapHeight = 30
+  mapWidth = 20
+  mapHeight = 60
 
   init(args: any) {
     this.state = args.game;
@@ -27,44 +27,17 @@ export default class HexMap extends Phaser.Scene {
     };
     this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
     this.cameras.main.setBackgroundColor('#fff');
-    const image = this.game.textures.get("hex_tiles").getSourceImage();
-    this.cameras.main.setScroll(-image.width - 40, -40);
+    this.cameras.main.setScroll(-40, -40);
 
-    const tileWidth = 32;
-    const tileHeight = 48;
-
-    const layerData = new Phaser.Tilemaps.LayerData({
-      tileWidth: tileWidth,
-      tileHeight: tileHeight,
-    });
-
-    const mapData = new Phaser.Tilemaps.MapData({
-      name: 'hexmap',
-      tileWidth: tileWidth,
-      tileHeight: tileHeight,
-      format: Phaser.Tilemaps.Formats.ARRAY_2D,
-      layers: [layerData],
-      orientation: 'hexagonal',
-    });
-
-    const tiles = [];
     for (let y = 0; y < this.mapHeight; y++) {
-      const row = []
       for (let x = 0; x < this.mapWidth; x++) {
-        row.push(new Phaser.Tilemaps.Tile(layerData, Math.floor(Math.random() * 20), x, y, tileWidth, tileHeight, tileWidth, tileHeight));
+        this.add.sprite(
+          x * 48 + ((y % 2 === 0) ? 0 : 24),
+          y * 13,
+          'hex_tiles',
+          Math.floor(Math.random() * 8));
       }
-      tiles.push(row);
     }
-    mapData.width = layerData.width = this.mapWidth;
-    mapData.height = layerData.height = this.mapHeight;
-    mapData.widthInPixels = layerData.widthInPixels = this.mapWidth * mapData.tileWidth;
-    mapData.heightInPixels = layerData.heightInPixels = this.mapHeight * mapData.tileHeight;
-    layerData.data = tiles;
-
-    const tilemap = new Phaser.Tilemaps.Tilemap(this, mapData);
-    console.log(tilemap);
-    const tileset = tilemap.addTilesetImage("hex_tiles", "hex_tiles", tileWidth, tileHeight, 0, 0);
-    tilemap.layers.forEach(layer => tilemap.createDynamicLayer(layer.name, tileset, 0, 0));
   }
 
   update(time: number, delta: number) {
--- src/scenes/Loading.ts
+++ src/scenes/Loading.ts
@@ -12,7 +12,10 @@ export default class Loading extends Phaser.Scene {
     this.load.image('dungeon', `${process.env.PUBLIC_URL}/images/dungeon.png`);
     this.load.image('characters', `${process.env.PUBLIC_URL}/images/characters.png`);
     this.load.image('general', `${process.env.PUBLIC_URL}/images/general.png`);
-    this.load.image('hex_tiles', `${process.env.PUBLIC_URL}/images/hex_tiles.png`);
+    this.load.spritesheet('hex_tiles', `${process.env.PUBLIC_URL}/images/hex_tiles.png`, {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
     this.load.loadComplete = () => {
       this.game.scene.start('HexMap', this.game);
     }
