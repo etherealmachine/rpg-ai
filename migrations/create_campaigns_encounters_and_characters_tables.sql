503ed4ed39a83635179450ad8c59da589e974a5c
James Pettit <james@academia.edu>
2020-05-13 14:24:57 +0800

    Sketch out campaigns, encounters, and characters tables.

---
+++ migrations/create_campaigns_encounters_and_characters_tables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE campaigns
+(
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX index_campaigns_on_owner_id on campaigns(owner_id);
+
+CREATE TABLE encounters
+(
+  id SERIAL PRIMARY KEY,
+  campaign_id INTEGER NOT NULL REFERENCES campaigns(id) ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  description TEXT,
+  tilemap_id INTEGER REFERENCES tilemaps(id) ON DELETE SET NULL,
+  events JSONB ARRAY,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX index_encounters_on_campaign_id on encounters(campaign_id);
+
+CREATE TABLE characters
+(
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  name TEXT NOT NULL DEFAULT 'Foo the Unnamed',
+  definition JSONB,
+  sprite BYTEA,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX index_characters_on_owner_id on characters(owner_id);
+
+CREATE TABLE campaign_characters
+(
+  id SERIAL PRIMARY KEY,
+  campaign_id INTEGER NOT NULL REFERENCES campaigns(id) ON DELETE CASCADE,
+  character_id INTEGER NOT NULL REFERENCES characters(id) ON DELETE CASCADE
+);
