588bbd5f9ac32be5f1160e6151691a3c9ae259f5
James Pettit <james@academia.edu>
2020-11-01 21:40:47 -0800

    Restart

--- reset_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dropdb `whoami`
-createdb `whoami`
-psql < migrations/create_users_table.sql
-psql < migrations/create_spritesheets_and_tilemaps_tables.sql
-psql < migrations/create_thumbnails_table.sql
--- server/generate.go
+++
@@ -1,3 +0,0 @@
-//go:generate qtc -dir=.
-//go:generate sqlc generate
-package main
