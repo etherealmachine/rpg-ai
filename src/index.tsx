2d4a8f2f15ecb5a51e1c31a88969853568e1e007
James Pettit <james@academia.edu>
2020-12-04 09:59:00 -0800

    Try out sentry for error tracing

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://d42a7c4cafcb47f5b298dbf6e44b1834@o486991.ingest.sentry.io/5545134",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
